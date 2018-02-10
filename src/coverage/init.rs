7b15a9464dfe8d3e74d531e5194eef8a95612360
= <phan.ngoclan58@gmail.com>
2018-02-10 14:54:17 +0700

    Fix initialization

--- src/coverage/init.rs
+++ src/coverage/init.rs
@@ -3,7 +3,7 @@ use rand;
 
 use coverage::models::*;
 
-pub fn random_init(conf: &Configuration, size: i32) -> Vec<Point> {
+pub fn random_points(conf: &Configuration, size: i32) -> Vec<Point> {
 	let mut state: Vec<Point> = Vec::new();
 	let mut r = rand::thread_rng();
 	let rngx = Range::new(0 as f64, conf.w as f64);
@@ -20,6 +20,20 @@ pub fn random_init(conf: &Configuration, size: i32) -> Vec<Point> {
 	state
 }
 
+pub fn random_state(conf: &Configuration) -> Vec<Point> {
+	random_points(&conf, conf.n)
+}
+
+pub fn random_init(conf: &Configuration, size: i32) -> Vec<Vec<Point>> {
+	let mut states: Vec<Vec<Point>> = Vec::new();
+	for _ in 0..size {
+		let s = random_state(&conf);
+		states.push(s);
+	}
+
+	states
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -28,8 +42,19 @@ mod tests {
 	fn test_random_init() {
 		let mut conf = Configuration::new();
 		conf.w = 100.; conf.h = 100.;
+		conf.n = 3;
 		let v = random_init(&conf, 20);
 		println!("{:?}", v);
 		assert_eq!(v.len(), 20);
+	}
+
+	#[test]
+	fn test_random_state() {
+		let mut conf = Configuration::new();
+		conf.w = 100.; conf.h = 100.;
+		conf.n = 10;
+		let v = random_state(&conf);
+		println!("{:?}", v);
+		assert_eq!(v.len(), conf.n as usize);
 	}
 }
